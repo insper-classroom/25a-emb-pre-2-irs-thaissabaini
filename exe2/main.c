265ecad6e08adb809b03a2a0d96b1db3264d98b4
thaissabaini <thaissc3@al.insper.edu.br>
2025-02-16 19:33:41 -0300

    exe2

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,28 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN = 28;
+const int LED_PIN = 4;
+volatile int btn_flag = 0;
+
+void btn_callback(uint gpio, uint32_t events) {
+  btn_flag = 1;
+}
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN);
+  gpio_set_dir(BTN_PIN, GPIO_IN);
+  gpio_pull_up(BTN_PIN);
+
+  gpio_init(LED_PIN);        
+  gpio_set_dir(LED_PIN, GPIO_OUT); 
+
   while (true) {
+    if (btn_flag){
+      btn_flag = 0;
+      gpio_put(LED_PIN, !gpio_get(LED_PIN));
+    }
   }
 }
