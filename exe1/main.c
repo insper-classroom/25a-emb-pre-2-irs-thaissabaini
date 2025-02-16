f2d511f66f154811436bfab5782f5a361c914359
thaissabaini <thaissc3@al.insper.edu.br>
2025-02-16 19:22:40 -0300

    update

--- exe1/main.c
+++ exe1/main.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
+volatile int btn_rise = 0;
+volatile int btn_fall = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
-    printf("fall \n");
+    btn_fall = 1;
   } else if (events == 0x8) { // rise edge
-    printf("rise \n");
+    btn_rise = 1;
   }
 }
 
@@ -23,5 +25,13 @@ int main() {
       BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
 
   while (true) {
+    if (btn_fall){
+      btn_fall = 0;
+      printf("fall \n");
+    }
+    if (btn_rise) {
+      btn_rise = 0;
+      printf("rise \n");
   }
+}
 }
